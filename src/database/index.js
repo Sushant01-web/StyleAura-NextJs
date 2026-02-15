cb7d87204ff2fdf45e013797a9ba39c04d8c291e
Sushant Gaikwad <sg123004@gmail.com>
2026-02-15 15:40:06 +0530

    Database updated

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,40 @@
+// import mongoose from "mongoose";
+
+// const configOptions = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
+
+// const connectToDB = async () => {
+//   const connectionUrl = process.env.MONGODB_URI
+
+//   mongoose
+//     .connect(connectionUrl, configOptions)
+//     .then(() => console.log("Ecommerce database connected successfully!"))
+//     .catch((err) =>
+//       console.log(`Getting Error from DB connection ${err.message}`)
+//     );
+// };
+
+// export default connectToDB;
+
+
 import mongoose from "mongoose";
 
-const configOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+let isConnected = false;
 
 const connectToDB = async () => {
-  const connectionUrl = process.env.MONGODB_URI
+  if (isConnected) {
+    return;
+  }
 
-  mongoose
-    .connect(connectionUrl, configOptions)
-    .then(() => console.log("Ecommerce database connected successfully!"))
-    .catch((err) =>
-      console.log(`Getting Error from DB connection ${err.message}`)
-    );
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = db.connections[0].readyState === 1;
+    console.log("Ecommerce database connected successfully!");
+  } catch (error) {
+    console.error("DB connection error:", error.message);
+  }
 };
 
 export default connectToDB;
